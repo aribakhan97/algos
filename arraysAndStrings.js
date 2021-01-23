b0af3fa50180446be135da8a79e8959f4fa098b7
aribakhan97 <akhan41@binghamton.edu>
2021-01-23 18:45:32 -0500

    more algos

--- arraysAndStrings.js
+++ arraysAndStrings.js
@@ -233,3 +233,15 @@ var threeSum = function(nums) {
 // O(n)2
 // space complexity --> O(1)
 
+
+
+// maxiumum subarray
+
+function maxSubArray (nums){
+    let solution = nums[0];
+    for(let i =1 ; i< nums.length; i++){
+        nums[i] = Math.max(nums[i], nums[i] + nums[i-1]);
+        solution = Math.max(solution, nums[i]);
+    }
+    return solution;
+};
