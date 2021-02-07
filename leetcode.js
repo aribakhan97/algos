c5b1e771ddd49afe575e3302c0ce0cb053dde4de
aribakhan97 <akhan41@binghamton.edu>
2021-02-06 19:34:50 -0500

    leetcode problems

---
+++ leetcode.js
@@ -0,0 +1,13 @@
+// remove duplicates from string 
+function removeDuplicates(nums) {
+    let numCount = 1;
+    const numsLen = nums.length;
+    for (let i = 1; i < numsLen; i++) {
+        if (nums[i] != nums[i - 1]) {
+            nums[numCount] = nums[i];
+            numCount++;
+        }
+    }
+    nums = nums.slice(0, numCount)
+    return numCount;
+}
