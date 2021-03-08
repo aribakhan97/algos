a1d0efb51d8ca22085e1602afd742c0c6e9c7e42
aribakhan97 <akhan41@binghamton.edu>
2021-03-07 22:54:13 -0500

    move element to end

--- Bloomberg/arraysAndStringsB.js
+++ Bloomberg/arraysAndStringsB.js
@@ -96,4 +96,20 @@ function smallestDifference(arrayOne, arrayTwo) {
 		}
 	}
 }
+// move element to end
 
+function moveElementToEnd(array, toMove) {
+	let i = 0;
+	let j = array.length - 1;
+	while (i < j) {
+			while ( i < j && array[j] === toMove) j--;
+			if (array[i] === toMove) swap(i, j, array);
+			i++
+		}
+		return array;
+	}
+	function swap(i, j, array){
+		const temp = array[j];
+		array[j] = array[i];
+		array[i] = temp;
+}
